49acd6f27fc6b7e939a1c1b786985fa28ab014c8
Igor Simões <igorsimoes_96@hotmail.com>
2024-03-18 10:49:38 +0000

    series model

---
+++ Movies&SeriesWebsite/Models/SeriesModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Movies_SeriesWebsite.Models
+{
+    public class SeriesModel
+    {
+        public int SerieID { get; set; }
+        public string SerieName { get; set; }
+        public int? StartYear { get; set; }
+        public int? EndYear { get; set; }
+        public int? OwnSeason { get; set; }
+        public int? LastSeason { get; set; }
+        public int? LastEpisode { get; set; }
+    }
+}
---
+++ Movies&SeriesWebsite/Pages/Series/SeriesList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Movies_SeriesWebsite.Pages.Series
+{
+    public class SeriesListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
